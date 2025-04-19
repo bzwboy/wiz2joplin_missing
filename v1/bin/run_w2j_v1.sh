b05bcfaa47107887f909a0983b84ef9ddecc842a
ott002 <ottmail@163.com>
2025-04-19 23:34:29 +0800

    [2025-04-19 23:34:29]

---
+++ v1/bin/run_w2j_v1.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+w2j -o ~/w2j -w ~/.wiznote -u mousecat4444@126.com -t 18dd9c058556c74ad5d26fc995e4705bc6b18769ca64129ddc7da61506727e6c0c579bc668adad1e914da26a7ec6621cb1b641cfb169fab6da419b16be057a03 -a -s
